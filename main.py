f2b982976961c3679a6a739ef897946a9b469a62
bjorn <bjorn@bjornkjellgren.se>
2022-03-14 10:05:19 +0100

    Övning 1

--- main.py
+++ main.py
@@ -3,3 +3,26 @@ import sqlite3
 # TODO anslut till databasen
 # TODO Skriv ut alla kunder
 # TODO Skriv ut alla ordrar
+
+LIST_CUSTOMERS = "SELECT customerno, first_name, last_name, street_name, town, zip, country, email, phone FROM customer"
+
+def main():
+    inp = ""
+    while inp != "quit":
+        inp = input(">")
+        if inp == "list customers":
+            con = sqlite3.connect("datastore/ovning.db")
+            cursor = con.cursor()
+            cursor.execute(LIST_CUSTOMERS)
+            for customer in cursor:
+                # TODO skriv ut kunddatan på ett snyggare sätt
+                print(customer)
+            con.close()
+        if inp == "list orders":
+            # TODO skriv ut ordrar
+            # ordernummer, kundnummer, orderdatum, förnamn, efternamn, adress
+            print("FIXME")
+
+
+if __name__ == '__main__':
+    main()
