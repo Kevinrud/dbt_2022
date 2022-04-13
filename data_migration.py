597e62c408133672bd4dbae0f5f688bf6a0256b6
bjorn <bjorn@bjornkjellgren.se>
2022-04-13 13:55:29 +0200

    Migration av data från MariaDB till Mongodb

--- data_migration.py
+++ data_migration.py
@@ -1,7 +1,6 @@
 import mariadb
 import pymongo
-
-
+import bson
 
 # Testa att ansluta mot MariaDB och läsa kunddata
 # 1. Ansluta
@@ -82,11 +81,43 @@ def migrate_customers():
         customers.insert_one(customer_dict)
     cur.close()
     connection.close()
+    client.close()
 
 
 # TODO Migrera produktdata från MariaDB till Mongodb
 #   Om ni får problem med decimalkonvertering kan istället konvertera priset till float
-#
+def migrate_products():
+    #  Migrera kunddata från MariaDB till Mongodb
+    # 1. Anslut till Mariadb
+    connection: mariadb.connection = mariadb.connect(
+        host=HOST,
+        port=PORT,
+        user=DBUSER,
+        password=DBPASS,
+        database=DBNAME
+    )
+    cur = connection.cursor()
+    client = pymongo.MongoClient('mongodb://localhost')
+    db = client['pvt21']
+    products = db['products']
+    # 2. Läs kunddata från Mariadb
+    cur.execute('SELECT * FROM products')
+    # 3. Bygg en python dict av varje produkt
+    for r in cur:
+        product_name = {
+            '_id': r[0],
+            'name': r[1],
+            'department': r[2],
+            'price': bson.Decimal128(r[3])
+        }
+        # 4. Spara kund-dicten i Mongodb
+        products.insert_one(product_name)
+    cur.close()
+    connection.close()
+    client.close()
+
+
+
 # TODO Migrera ordrar från MariaDB till Mongodb
 #   TODO Ta fram en struktur för orderdata i Mongodb, hur vill vi att datan
 #    skall se ut
